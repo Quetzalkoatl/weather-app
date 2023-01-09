24b89e061acf3c9e75d9d520a4006603233dd9ce
SavelevSergey <itsgotrunn@gmail.com>
2023-01-09 20:45:35 +0300

    more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IWeatherData } from './models/weather.model';
 import { WeatherService } from './services/weather.service';
 
-import { fakeData } from 'fakeData';
+// import { fakeData } from 'fakeData';
 
 @Component({
   selector: 'app-root',
@@ -11,37 +11,37 @@ import { fakeData } from 'fakeData';
 })
 export class AppComponent implements OnInit {
   cityName: string = 'Moscow';
-  wrongCityName: boolean = false;
+  wrongCityName: boolean = true;
   weatherData!: IWeatherData;
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    // this.getWeatherData(this.cityName);
-    this.weatherData = fakeData;
+    this.getWeatherData(this.cityName);
+    // this.weatherData = fakeData;
   }
 
   onSubmit() {
-    // this.getWeatherData(this.cityName);
+    this.getWeatherData(this.cityName);
   }
 
-  // private getWeatherData(cityName: string) {
-  //   this.weatherService.getWeatherData(cityName).subscribe({
-  //     next: (response) => {
-  //       if (
-  //         response.location.name &&
-  //         cityName.toLowerCase() === response.location.name.toLowerCase()
-  //       ) {
-  //         this.weatherData = response;
-  //         this.wrongCityName = false;
-  //       } else {
-  //         this.wrongCityName = true;
-  //       }
-  //     },
-  //     error: (error) => {
-  //       this.wrongCityName = true;
-  //       console.log(error);
-  //     },
-  //   });
-  // }
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        if (
+          response.location.name &&
+          cityName.toLowerCase() === response.location.name.toLowerCase()
+        ) {
+          this.weatherData = response;
+          this.wrongCityName = false;
+        } else {
+          this.wrongCityName = true;
+        }
+      },
+      error: (error) => {
+        this.wrongCityName = true;
+        console.log(error);
+      },
+    });
+  }
 }
