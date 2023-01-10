6c148168c569c52d2c7973fbda3ecb86892e4838
SavelevSergey <itsgotrunn@gmail.com>
2023-01-10 07:01:30 +0300

    build

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  weatherApiBaseUrl: 'https://api.weatherapi.com/v1/current.json?key=',
-  key: '2317f449e4af44f3bad65207230901',
+  weatherApiBaseUrl: 'BASE_URL',
+  key: 'KEY',
 };
